e64a13bd68678aa0ea86e1889b2e98cab1df4fe5
Thilo Molitor <thilo@monal-im.org>
2024-11-09 04:48:42 +0100

    Fix backported code

--- Monal/Classes/ActiveChatsViewController.m
+++ Monal/Classes/ActiveChatsViewController.m
@@ -366,8 +366,8 @@ static NSMutableSet* _pushWarningDisplayed;
             BOOL found = NO;
             for(NSDictionary* accountDict in [[DataLayer sharedInstance] enabledAccountList])
             {
-                NSNumber* accountID = accountDict[kAccountID];
-                if([MLNotificationManager sharedInstance].currentContact.accountID.intValue == accountID.intValue)
+                NSNumber* accountId = accountDict[kAccountID];
+                if([MLNotificationManager sharedInstance].currentContact.accountId.intValue == accountId.intValue)
                     found = YES;
             }
             if(!found)
